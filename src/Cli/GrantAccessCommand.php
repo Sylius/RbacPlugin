ff4114bc7effc7b74f14c6b0855d4fecd3fff55b
Bartosz Pietrzak <bartosz.pietrzak1994@gmail.com>
2018-11-28 15:47:26 +0100

    ECS fixes

--- src/Cli/GrantAccessCommand.php
+++ src/Cli/GrantAccessCommand.php
@@ -16,7 +16,6 @@ use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
-use Webmozart\Assert\Assert;
 
 final class GrantAccessCommand extends Command
 {
