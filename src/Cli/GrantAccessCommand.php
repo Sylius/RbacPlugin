9c2be64581cbec3569c0d571ef4672e561300b4a
Bartosz Pietrzak <bartosz.pietrzak1994@gmail.com>
2018-11-28 14:34:20 +0100

    Support for multi section and role assigment v2

---
+++ src/Cli/GrantAccessCommand.php
@@ -0,0 +1,90 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Sylius\RbacPlugin\Cli;
+
+use Doctrine\Common\Persistence\ObjectManager;
+use Sylius\Component\Resource\Repository\RepositoryInterface;
+use Sylius\RbacPlugin\Access\Model\OperationType;
+use Sylius\RbacPlugin\Entity\AdministrationRole;
+use Sylius\RbacPlugin\Entity\AdministrationRoleInterface;
+use Sylius\RbacPlugin\Entity\AdminUserInterface;
+use Sylius\RbacPlugin\Model\Permission;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Webmozart\Assert\Assert;
+
+final class GrantAccessCommand extends Command
+{
+    /** @var RepositoryInterface */
+    private $administratorRepository;
+
+    /** @var RepositoryInterface */
+    private $administratorRoleRepository;
+
+    /** @var ObjectManager */
+    private $objectManager;
+
+    public function __construct(
+        RepositoryInterface $administratorRepository,
+        RepositoryInterface $administratorRoleRepository,
+        ObjectManager $objectManager
+    ) {
+        parent::__construct('sylius-rbac:grant-access');
+
+        $this->administratorRepository = $administratorRepository;
+        $this->administratorRoleRepository = $administratorRoleRepository;
+        $this->objectManager = $objectManager;
+    }
+
+    protected function configure()
+    {
+        $this
+            ->setDescription('Grants access to chooseable sections for administrator')
+            ->addArgument('email', InputOption::VALUE_REQUIRED)
+            ->addArgument('roleName', InputOption::VALUE_REQUIRED)
+            ->addArgument('sections', InputArgument::IS_ARRAY | InputArgument::REQUIRED)
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        /** @var AdminUserInterface $admin */
+        $admin = $this->administratorRepository->findOneBy(['email' => $input->getArgument('email')]);
+        $roleName = $input->getArgument('roleName');
+
+        Assert::string($roleName);
+
+        $configuratorRole = $this->getOrCreateConfiguratorRole($roleName);
+
+        /** @var array $sections */
+        $sections = $input->getArgument('sections');
+
+        foreach ($sections as $section) {
+            $configuratorRole->addPermission(Permission::$section([OperationType::read(), OperationType::write()]));
+        }
+
+        $this->administratorRoleRepository->add($configuratorRole);
+        $admin->setAdministrationRole($configuratorRole);
+
+        $this->objectManager->flush();
+    }
+
+    private function getOrCreateConfiguratorRole(string $roleName): AdministrationRoleInterface
+    {
+        // This behaviour is debatable - either just override the selected role or throw an exception.
+        /** @var AdministrationRoleInterface|null $configuratorRole */
+        $configuratorRole = $this->administratorRoleRepository->findOneBy(['name' => $roleName]);
+
+        if (null === $configuratorRole) {
+            $configuratorRole = new AdministrationRole();
+            $configuratorRole->setName($roleName);
+        }
+
+        return $configuratorRole;
+    }
+}
