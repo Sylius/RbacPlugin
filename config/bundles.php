3b50d558272d93cddecffc20e6cbabd3370b466f
Bartosz Pietrzak <bartosz.pietrzak1994@gmail.com>
2019-02-05 15:31:09 +0100

    RBAC plugin compatible with Sylius 1.4

---
+++ config/bundles.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    Nelmio\Alice\Bridge\Symfony\NelmioAliceBundle::class => ['dev' => true, 'test' => true],
+    Fidry\AliceDataFixtures\Bridge\Symfony\FidryAliceDataFixturesBundle::class => ['dev' => true, 'test' => true],
+];
