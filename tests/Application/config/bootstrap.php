dea2309f0d172ed8e7a5c54942fd8e0d520fb525
Bartosz Pietrzak <bartosz.pietrzak1994@gmail.com>
2019-02-05 16:32:05 +0100

    ECS fixes

--- tests/Application/config/bootstrap.php
+++ tests/Application/config/bootstrap.php
@@ -1,9 +1,6 @@
 <?php
-
 use Symfony\Component\Dotenv\Dotenv;
-
 require dirname(__DIR__).'../../../vendor/autoload.php';
-
 // Load cached env vars if the .env.local.php file exists
 // Run "composer dump-env prod" to create it (requires symfony/flex >=1.2)
 if (is_array($env = @include dirname(__DIR__).'/.env.local.php')) {
@@ -15,7 +12,6 @@ if (is_array($env = @include dirname(__DIR__).'/.env.local.php')) {
     // load all the .env files
     (new Dotenv())->loadEnv(dirname(__DIR__).'/.env');
 }
-
 $_SERVER['APP_ENV'] = $_ENV['APP_ENV'] = ($_SERVER['APP_ENV'] ?? $_ENV['APP_ENV'] ?? null) ?: 'dev';
 $_SERVER['APP_DEBUG'] = $_SERVER['APP_DEBUG'] ?? $_ENV['APP_DEBUG'] ?? 'prod' !== $_SERVER['APP_ENV'];
 $_SERVER['APP_DEBUG'] = $_ENV['APP_DEBUG'] = (int) $_SERVER['APP_DEBUG'] || filter_var($_SERVER['APP_DEBUG'], FILTER_VALIDATE_BOOLEAN) ? '1' : '0';
