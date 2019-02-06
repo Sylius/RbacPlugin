75e10c221343dcbf1a319eea5ea19bc721f59423
Bartosz Pietrzak <bartosz.pietrzak1994@gmail.com>
2019-02-06 12:55:30 +0100

    Review fixes

--- config/bundles.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-return [
-    Nelmio\Alice\Bridge\Symfony\NelmioAliceBundle::class => ['dev' => true, 'test' => true],
-    Fidry\AliceDataFixtures\Bridge\Symfony\FidryAliceDataFixturesBundle::class => ['dev' => true, 'test' => true],
-];
